b8f9f13b3187e48ed18d50f32de0aa91bb64dcd5
Wei Fang <fang.wei@dena.jp>
2015-08-27 19:03:36 +0900

    add OrgChart.

---
+++ OrgChart/OrgChart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OrgChart
+//
+//  Created by Wei Fang on 8/27/15.
+//  Copyright (c) 2015 Wei Fang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OrgChart/OrgChart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OrgChart
+//
+//  Created by Wei Fang on 8/27/15.
+//  Copyright (c) 2015 Wei Fang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
